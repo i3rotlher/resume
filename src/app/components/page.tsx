3c39479ba4c2fd7cef1548c316ca801d4b0762cf
skilli.maschter@gmail.com <skilli.maschter@gmail.com>
2024-06-18 16:54:58 +0900

    initial commit, globalstyle

---
+++ src/app/components/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ComponentTest = () => {
+  return (
+    <div>
+      <h1 className={"strokedWhiteText"}>Hands on Projects</h1>
+      <h3>Hello bla bla</h3>
+    </div>
+  );
+};
+
+export default ComponentTest;
