4b154adeee552b1129be3115f88726eb92555c09
skilli.maschter@gmail.com <skilli.maschter@gmail.com>
2024-06-28 22:38:16 +0900

    layout done

--- src/app/components/sections/LeftSection.tsx
+++ src/app/components/sections/LeftSection.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import styles from "../../styles/sections/LeftSection.module.css";
 import Section from "./Section";
 import Image from "next/image";
+import ProgressBar from "../util/ProgressBar";
 
 const LeftSection = () => {
   function nameRow() {
     return (
-      <div className={styles.nameRow}>
-        <h1>Name:</h1>
+      <div className={`${styles.row}`}>
+        <h1 className={styles.title}>Name:</h1>
         <h1 className={styles.field + " strokedWhiteTextHighContrast"}>Max</h1>
       </div>
     );
@@ -15,17 +16,17 @@ const LeftSection = () => {
 
   function levelRow() {
     return (
-      <div className={styles.levelRow}>
-        <h2>Level:</h2>
-        <h2>here is going to the Progressbar</h2>
+      <div className={`${styles.levelRow}`}>
+        <h2 className={styles.levelTitle}>Level:</h2>
+        <ProgressBar percent={daysSinceLastDec23()} label={"25"}></ProgressBar>
       </div>
     );
   }
 
   function classRow() {
     return (
-      <div className={styles.nameRow}>
-        <h2>Class:</h2>
+      <div className={`${styles.row}`}>
+        <h2 className={styles.title}>Class:</h2>
         <h2 className={styles.field + " strokedWhiteTextHighContrast"}>
           Fullstack Developer
         </h2>
@@ -35,8 +36,8 @@ const LeftSection = () => {
 
   function nationRow() {
     return (
-      <div className={styles.nameRow}>
-        <h2>Nation:</h2>
+      <div className={`${styles.row}`}>
+        <h2 className={styles.title}>Nation:</h2>
         <h2 className={styles.field + " strokedWhiteTextHighContrast"}>
           German
         </h2>
@@ -44,22 +45,48 @@ const LeftSection = () => {
     );
   }
 
+  function uglyFace() {
+    return (
+      <Image
+        src="/uglyFace.png"
+        alt="Handsome Boy"
+        fill={true}
+        style={{ objectFit: "contain" }}
+        layout="resposive"
+      />
+    );
+  }
+
+  function daysSinceLastDec23(): number {
+    const now: Date = new Date();
+    const currentYear: number = now.getFullYear();
+    let lastDec23: Date = new Date(currentYear - 1, 11, 23); // Last year's Dec 23rd
+
+    if (now >= new Date(currentYear, 11, 23)) {
+      // If the current date is on or after this year's Dec 23rd
+      lastDec23.setFullYear(currentYear); // Use this year's Dec 23rd
+    }
+
+    const nextDec23: Date = new Date(lastDec23.getFullYear() + 1, 11, 23); // Next Dec 23rd
+    const daysInYear: number =
+      (nextDec23.getTime() - lastDec23.getTime()) / (1000 * 60 * 60 * 24); // Total days between the two Dec 23rds
+    const daysCompleted: number =
+      (now.getTime() - lastDec23.getTime()) / (1000 * 60 * 60 * 24); // Days completed since last Dec 23rd
+
+    const result: number = daysCompleted / daysInYear;
+    return parseFloat(result.toFixed(2));
+  }
+
   return (
     <Section>
       <div className={styles.content}>
-        {nameRow()}
-        {levelRow()}
-        {classRow()}
-        {nationRow()}
-        <div className={styles.uglyFaceContainer}>
-          <Image
-            src="/uglyFace.png"
-            alt="Handsome Boy"
-            fill={true}
-            style={{ objectFit: "contain" }}
-            layout="resposive"
-          />
+        <div className={styles.fieldsContainer}>
+          {nameRow()}
+          {levelRow()}
+          {classRow()}
+          {nationRow()}
         </div>
+        <div className={styles.uglyFaceContainer}>{uglyFace()}</div>
       </div>
     </Section>
   );
---
+++ src/app/components/util/ProgressBar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import style from "../../styles/util/ProgressBar.module.css";
+
+type ProgressBarProps = {
+  percent: number;
+  label: String;
+};
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ percent, label }) => {
+  return (
+    <div className={style.progressbarBackground}>
+      <div
+        className={style.progressbar}
+        style={{ width: `${percent * 100}%` }}
+      />
+      <h2 className={`${style.label} strokedWhiteTextHighContrast`}>{label}</h2>{" "}
+    </div>
+  );
+};
+
+export default ProgressBar;
