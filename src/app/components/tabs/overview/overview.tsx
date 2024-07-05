acba14b9707907c08da49b80e8b5e8e47f86b539
skilli.maschter@gmail.com <skilli.maschter@gmail.com>
2024-07-05 22:39:15 +0900

    added framedText as a component

--- src/app/components/tabs/overview/overview.tsx
+++ src/app/components/tabs/overview/overview.tsx
@@ -1,8 +1,26 @@
+import style from "../../../styles/tabs/overview.module.css";
+import { TextWithFrame } from "../../util/TextWithFrame";
 const Overview = () => {
   return (
-    <>
-      <p>This is the overview content.</p>
-    </>
+    <div className={style.content}>
+      <div className={style.description}>
+        <TextWithFrame>
+          <h2 style={{ textAlign: "justify" }}>
+            Max, the Fullstack Developer, is a true master of digital realms.
+            With his ability to seamlessly blend frontend and backend
+            technologies, he is akin to a powerful mage proficient in both
+            arcane arts and swordsmanship. His tools are not sword and shield,
+            but keyboard and mouse, and his battlefield is the code. Max, the
+            Fullstack Developer, is a true master of digital realms. With his
+            ability to seamlessly blend frontend and backend technologies, he is
+            akin to a powerful mage proficient in both arcane arts and
+            swordsmanship. His tools are not sword and shield, but keyboard and
+            mouse, and his battlefield is the code.
+          </h2>
+        </TextWithFrame>
+      </div>
+      <div className={style.skills}>Skills</div>
+    </div>
   );
 };
 
---
+++ src/app/components/util/TextWithFrame.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from "react";
+import Image from "next/image";
+import style from "../../styles/util/TextWithFrame.module.css";
+
+type TextProps = {
+  children: ReactNode;
+};
+
+export const TextWithFrame: React.FC<TextProps> = ({ children }) => {
+  return (
+    <div className={`${style.textContainer} strokedWhiteText`}>
+      <div className={style.paddedText}> {children}</div>
+      <Image src="/frame_horizontal.png" alt="" fill className={style.frame} />
+    </div>
+  );
+};
