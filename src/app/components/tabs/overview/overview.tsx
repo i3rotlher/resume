2e1d1f9c39f5255ed906403965ee361c7536de81
skilli.maschter@gmail.com <skilli.maschter@gmail.com>
2024-07-06 15:06:41 +0900

    overview done

--- src/app/components/tabs/overview/overview.tsx
+++ src/app/components/tabs/overview/overview.tsx
@@ -1,12 +1,14 @@
+import Image from "next/image";
 import style from "../../../styles/tabs/overview.module.css";
 import ProgressBar from "../../util/ProgressBar";
 import { TextWithFrame } from "../../util/TextWithFrame";
+import { url } from "inspector";
 const Overview = () => {
   return (
     <div className={style.content}>
       <div className={style.description}>
         <TextWithFrame>
-          <h2 style={{ textAlign: "justify" }}>
+          <h2 style={{ textAlign: "justify", lineHeight: "120%" }}>
             Max, the Fullstack Developer, is a true master of digital realms.
             With his ability to seamlessly blend frontend and backend
             technologies, he is akin to a powerful mage proficient in both
@@ -15,18 +17,30 @@ const Overview = () => {
           </h2>
         </TextWithFrame>
       </div>
-      <div className={style.skills}>
-        <h2>Attributes:</h2>
-        <ProgressBar
-          percent={70 / 100}
-          label="Frontend 70/100"
-          color="#CD5B58"
-        />
-        <ProgressBar
-          percent={90 / 100}
-          label="Backend 90/100"
-          color="#7B99E7"
-        />
+      <h2>Abillities:</h2>
+      <div className={style.skillsRow}>
+        <div className={style.skillIcon}>
+          <Image src="/frontend_icon.svg" alt="" fill />
+        </div>
+        <div className={style.skillProgressbar}>
+          <ProgressBar
+            percent={70 / 100}
+            label="Frontend 70/100"
+            color="#7B99E7"
+          />
+        </div>
+      </div>
+      <div className={style.skillsRow}>
+        <div className={style.skillIcon}>
+          <Image src="/backend_icon.svg" alt="" fill />
+        </div>
+        <div className={style.skillProgressbar}>
+          <ProgressBar
+            percent={90 / 100}
+            label="Backend 90/100"
+            color="#CD5B58"
+          />
+        </div>
       </div>
     </div>
   );
