eb9738d6f890f009154d1abc5033d2da45716000
skilli.maschter@gmail.com <skilli.maschter@gmail.com>
2024-07-10 17:35:08 +0900

    devider added

--- src/app/components/util/Devider.tsx
+++ src/app/components/util/Devider.tsx
@@ -3,9 +3,47 @@ import Image from "next/image";
 
 export const Devider = () => {
   return (
-    <div style={{ position: "relative", height: "100%" }}>
-      {" "}
-      <Image src="/devider.svg" alt="" fill></Image>
+    <div style={{ height: "100%", display: "flex" }}>
+      <div
+        style={{
+          position: "relative",
+          height: "100%",
+          flex: "1",
+          objectFit: "contain",
+          display: "flex",
+          alignItems: "end",
+        }}
+      >
+        <Image src="/devider/devider_twirl_tr.svg" alt="" fill></Image>
+      </div>
+      <div
+        style={{
+          position: "relative",
+          height: "100%",
+          flex: "32",
+        }}
+      >
+        <div
+          style={{
+            background: "url(/devider/devider_middle.svg)",
+            height: "100%",
+            width: "100%",
+            objectFit: "contain",
+            backgroundPosition: "center",
+            backgroundRepeat: "repeat-x",
+          }}
+        ></div>
+      </div>
+      <div
+        style={{
+          position: "relative",
+          height: "100%",
+          flex: "1",
+          objectFit: "contain",
+        }}
+      >
+        <Image src="/devider/devider_twirl_bl.svg" alt="" fill></Image>
+      </div>
     </div>
   );
 };
