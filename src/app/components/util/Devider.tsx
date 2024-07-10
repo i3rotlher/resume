9d9973bc823d9864bbc69082db74feb1df54d893
skilli.maschter@gmail.com <skilli.maschter@gmail.com>
2024-07-10 18:10:29 +0900

    devider bug fixed when tiny height

--- src/app/components/util/Devider.tsx
+++ src/app/components/util/Devider.tsx
@@ -14,25 +14,38 @@ export const Devider = () => {
           alignItems: "end",
         }}
       >
-        <Image src="/devider/devider_twirl_tr.svg" alt="" fill></Image>
+        <Image src="/devider/devider_twirl_tl.svg" alt="" fill></Image>
       </div>
       <div
         style={{
           position: "relative",
           height: "100%",
           flex: "32",
+          width: "100%",
         }}
       >
-        <div
-          style={{
-            background: "url(/devider/devider_middle.svg)",
-            height: "100%",
-            width: "100%",
-            objectFit: "contain",
-            backgroundPosition: "center",
-            backgroundRepeat: "repeat-x",
-          }}
-        ></div>
+        <svg
+          width="100%"
+          height="100%"
+          viewBox="0 0 737 38"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+        >
+          <defs>
+            <pattern
+              id="pattern"
+              x="0"
+              y="0"
+              width="737"
+              height="38"
+              patternUnits="userSpaceOnUse"
+            >
+              <path d="M0 19H737" stroke="#BC894D" strokeWidth="2" />
+            </pattern>
+          </defs>
+          <rect x="0" y="0" width="100%" height="100%" fill="url(#pattern)" />
+        </svg>
       </div>
       <div
         style={{
@@ -42,7 +55,7 @@ export const Devider = () => {
           objectFit: "contain",
         }}
       >
-        <Image src="/devider/devider_twirl_bl.svg" alt="" fill></Image>
+        <Image src="/devider/devider_twirl_br.svg" alt="" fill></Image>
       </div>
     </div>
   );
