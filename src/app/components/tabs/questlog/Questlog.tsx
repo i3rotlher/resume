c7f739921be84bbe5b47f394c7a6e566313592d0
skilli.maschter@gmail.com <64362712+i3rotlher@users.noreply.github.com>
2024-10-09 19:08:06 +0200

    fixed questlog

---
+++ src/app/components/tabs/questlog/Questlog.tsx
@@ -0,0 +1,16 @@
+import { QuestCard } from "../../util/QuestCard";
+import style from "../../../styles/tabs/Questlog.module.css";
+import { Quests } from "./QuestlogData";
+const QuestLog = () => {
+  return (
+    <div className={style.hideScrollBar}>
+      <div className={style.questlogContainer}>
+        {Quests.map((quest, index) => (
+          <QuestCard {...quest} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestLog;
